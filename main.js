9e1d48e856e72aea6f0aac2e245461d9a8981e95
Yevhenii-dvurechenskyi <dvurecenskijzena61@gmail.com>
2022-04-15 14:26:38 +0300

    fixet class TodoItem

--- main.js
+++ main.js
@@ -1,33 +1,35 @@
-class Todoitem{
-title;
-description;
-
-    createTodoItem(id){
-        let objTodo = {
-            id: this.id = id,
-            title: this.title = prompt("Enter your title: ", "none"),
-            description: this.description = prompt("Enter your description: ", "none"),
-            done: false,
-        }
-        return objTodo;
-    };
+class TodoItem{
+    done;
+    constructor(id){
+        this.id = id;
+        this.title = prompt("Enter your title: ", "none");
+        this.description = prompt("Enter your description: ", "none");
+        this.done = false;
+    }
 }
 class TodoList {
-
     items = [];
 
-    todo = new Todoitem();
     add(operations){
         let i = 0;
         while(operations > i){
-            this.items.push(this.todo.createTodoItem(++i));
+            this.items.push(new TodoItem(++i));
+        }
+        let swicherCompleteAll = confirm(`Do all the things on the list?`);
+        if(swicherCompleteAll === true){
+            this.items.push(this.completeAll());
         }
     }
+
     completeAll(){
         for(let i = 0; i < this.items.length; i++){
+            if(i <= this.items.length - 1){
             this.items[i].done = true;
+            }
+            else{break;} 
         }
-    };
+    }
+
     display(){
         for(let i = 0; i < this.items.length; i++){
             if(this.items[i] !== undefined){
@@ -38,7 +40,6 @@ class TodoList {
 }
 
 let todoList = new TodoList();
-todoList.add(3);
+todoList.add(2);
 todoList.display();
-todoList.items.push(todoList.completeAll());
-todoList.display();
+
